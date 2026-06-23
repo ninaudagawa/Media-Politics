639358bf58e47db8f6fdaeec6d5c9ff0ed0eb76a
cyrusncw05 <cyrusncw05@gmail.com>
2026-06-23 12:11:11 +0900

    i made the keyword.py file

---
+++ project/notebooks/keywords.py
@@ -0,0 +1,26 @@
+
+CATEGORY_KEYWORDS = {
+    "defense": {
+        "defense", "defence", "military", "self-defense", "self-defence",
+        "security", "troops", "army", "navy", "armed forces", "war", "SDF",
+        "missile", "military base", "alliance", "deterrence", "article 9", "threat"
+    },
+    "economy": {
+        "economy", "economic", "trade", "tax", "budget", "growth",
+        "inflation", "employment", "wage", "industry", "investment",
+        "market", "finance", "financial", "gdp", "deficit", "export",
+        "import", "business", "company", "stimulus", "interest rate", "exchange rate", 
+        "Bank of Japan"
+    },
+    "nuclear": {
+        "nuclear", "atomic", "reactor", "radiation", "fukushima",
+        "non-proliferation", "weapons", "plutonium", "uranium",
+        "power plant",
+    },
+    "immigration": {
+        "immigration", "immigrant", "refugee", "migrant", "migration",
+        "asylum", "border", "foreign worker", "visa", "naturalization",
+        "nationalisation", "nationalization", "permanent resident",
+        "foreigner"
+    },
+}
