f7f70a712933069f70bde57af1af78283be3c156
akbalke <ailie.balke@gmail.com>
2026-06-24 12:13:25 +0900

    Updated my graphs, notebook complete
    Updated my graphs, notebook complete

--- project/notebooks/keywords.py
+++ project/notebooks/keywords.py
@@ -3,7 +3,7 @@ CATEGORY_KEYWORDS = {
     "defense": {
         "defense", "defence", "military", "self-defense", "self-defence",
         "security", "troops", "army", "navy", "armed forces", "war", "SDF",
-        "missile", "military base", "alliance", "deterrence", "article 9", "threat", "north korea", "china", "russia", 
+        "missile", "military base", "alliance", "deterrence", "article 9", "threat", "north korea", "china", "russia", "security council"
     },
     "economy": {
         "economy", "economic", "trade", "tax", "budget", "growth",
@@ -15,7 +15,7 @@ CATEGORY_KEYWORDS = {
     "nuclear": {
         "nuclear", "atomic", "reactor", "radiation", "fukushima",
         "non-proliferation", "weapons", "plutonium", "uranium",
-        "power plant", "nuclear weapons", "world free of nuclear weapons", "atomic bomb survivors"
+        "power plant", "nuclear weapons", "world free of nuclear weapons", "atomic bomb survivors", "nuclear disarmament"
     },
     "immigration": {
         "immigration", "immigrant", "refugee", "migrant", "migration",
