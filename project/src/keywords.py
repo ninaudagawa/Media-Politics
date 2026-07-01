4c8f142283a2c7364824d9000e054f30c0334631
akbalke <ailie.balke@gmail.com>
2026-07-01 14:03:37 +0900

    Updated keywords from ollama notebook

--- project/src/keywords.py
+++ project/src/keywords.py
@@ -1,25 +1,21 @@
 CATEGORY_KEYWORDS = {
     "defense": {
-        "defense", "defence", "military", "self-defense", "self-defence",
-        "troops", "army", "navy", "armed forces", "SDF",
-        "missile", "military base", "alliance", "deterrence", "article 9", "threat", "north korea", "china", "russia", "security council"
+        "self defense", "self defence", "ASEAN", "US-Japan Alliance", 
+        "NATO", "Triton", "triton", "Maritime Security","maritime security", "DFNS", "Intelligence-Led Operations",
+        "Cybersecurity", "cybersecurity", "Alliance Deterrence" "alliance deterrence",
     },
-    "economy": {
-        "economy", "economic", "trade", "tax", "budget", "growth",
-        "inflation", "employment", "wage", "industry", "investment",
-        "market", "finance", "financial", "gdp", "deficit", "export",
-        "import", "business", "company", "stimulus", "interest rate", "exchange rate", 
-        "Bank of Japan", "economic growth", "financial contributions"
+    "economy": {"Abenomics", "stimulus", "monetization", "monetisation", "deflationary", "Abe doctrine", "sustainable growth", "structural reform", 
+                "export-led growth", "demographic shift", "stable growth", "Abenomics", "Stimulus", "Monetization", "Deflationary", 
+                "Abe doctrine", "Sustainable growth", "Structural reform", "Export-led growth", "Demographic shift", "Stable growth"
     },
     "nuclear": {
-        "nuclear", "atomic", "reactor", "radiation", "fukushima",
-        "non-proliferation", "weapons", "plutonium", "uranium",
-        "power plant", "nuclear weapons", "world free of nuclear weapons", "atomic bomb survivors", "nuclear disarmament"
+        "Deterrence", "Non-proliferation", "Alliance", "Self-defense", "NPT", "SDGT", 
+        "Takeshima", "ASEAN", "US-Japan Security Treaty", "Peaceful use", "deterrence",
+        "non-proliferation", "alliance", "self-defense",  "peaceful use"
     },
     "immigration": {
-        "immigration", "immigrant", "refugee", "migrant", "migration",
-        "asylum", "border", "foreign worker", "visa", "naturalization",
-        "nationalisation", "nationalization", "permanent resident",
-        "foreigner"
+        "Quota", "Skilled", "Guestworker", "Refugee", "Integration", 
+        "Citizenship", "Asylum", "Pathway", "Talent", "Inclusion", "quota", "skilled", "guestworker", "refugee", "integration", 
+        "citizenship", "asylum", "pathway", "talent", "inclusion"
     },
 }
